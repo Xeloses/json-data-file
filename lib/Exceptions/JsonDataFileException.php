dcf497887c8b59ee65efbf33d46467706edf08fe
Xeloses <xeloses@gmail.com>
2021-11-16 15:56:01 +0300

    fix spelling

--- lib/Exceptions/JsonDataFileException.php
+++ lib/Exceptions/JsonDataFileException.php
@@ -7,7 +7,7 @@
  * @package    JsonDataFile (https://github.com/Xeloses/json-data-file)
  * @version    1.0.2
  * @copyright  Xeloses 2018-2020
- * @license    MIT (http://en.wikipedia.org/wiki/MIT_License)
+ * @license    GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.html)
  */
 
 namespace Xeloses\JsonDataFile\Exceptions;
